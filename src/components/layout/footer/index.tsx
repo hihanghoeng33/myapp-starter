a2a60a2d5e9e48daf6bdf68a9abcc30df51a879e
Ardanatha <142950868+hihanghoeng33@users.noreply.github.com>
2025-02-16 00:55:57 +0700

    Minor change index.tsx

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="bg-gray h-12 text-white mt-9 text-center">&copy made by hihanghoeng. All right reserved.</div>
+        <div className="bg-gray h-12 text-white mt-9 text-center">&copy; made by hihanghoeng. All right reserved.</div>
     )
 }
 
